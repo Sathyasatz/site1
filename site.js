2cf975a7cc4ba6fcff1f0f01037874d1117e23e5
Sathiya Naarayanan <bals@healthstandards.org>
2023-10-12 13:37:26 -0400

    Init commit

---
+++ site.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+//serves homeSite.html and sets first-party cookie
+app.get('/', function (req, res) {
+
+    res.cookie("first-party","785");
+    res.sendFile(path.join(__dirname+"/home.html"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
